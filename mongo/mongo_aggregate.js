0b58a88338e2d54c67783575f5e6eed197bb3b0e
Matías Hernán García <mati92.ss@gmail.com>
2014-10-29 00:38:56 -0300

    Update mongo_aggregate.js
    Add missing ; and siblingDB

--- mongo/mongo_aggregate.js
+++ mongo/mongo_aggregate.js
@@ -1,3 +1,4 @@
+db = db.getSiblingDB("GestionClub");
 //1) Realizar una consulta que devuelva la siguiente información: deporte y total de ingresos por deporte.
 db.socios.aggregate([ 
 	{ $unwind: "$deportes" }, 
@@ -6,7 +7,7 @@ db.socios.aggregate([
 		{_id:"$deportes", 
 		total: {$sum: "$cuotas.importe"}
 	}}
-])
+]);
 
 //3)Basado en la consulta del punto 1, mostrar sólo los deportes que tengan un ingreso superior a un determinado valor (elegimos $900)	.
 db.socios.aggregate([ 
@@ -17,7 +18,7 @@ db.socios.aggregate([
 		total: {$sum: "$cuotas.importe"}
 	}},
 	{ $match: {total: {$gt: 900 }} }
-])
+]);
 
 /*4)Se requiere obtener un reporte que contenga la siguiente información, nro de socio, apellido y nombre y cantidad de deportes que practica, 
 ordenado por apellido.*/
@@ -28,7 +29,7 @@ db.socios.aggregate([
 		count: {$sum: 1}
 	}},
 	{ $sort: {"_id.apellido": 1}}
-])
+]);
 
 /*5)Basados en la consulta del punto 4 informar sólo los socios cuyo número de socio es mayor o igual a 120000 (en el caso de no mostrar resultados, 
 poner un número acorde a los datos que poseen).*/
@@ -40,7 +41,7 @@ db.socios.aggregate([
 		count: {$sum: 1}
 	}},
 	{ $sort: {"_id.apellido": 1}}
-])
+]);
 
 /*6)Basados en la consulta del punto 5 informar, informar sólamente la cantidad de socios que cumplen con esta condición.*/
 db.socios.aggregate([ 
@@ -53,7 +54,7 @@ db.socios.aggregate([
 		{_id: null,
 		count: {$sum: 1}}
 	}
-])
+]);
 
 /*7)Se requiere realizar una consulta que devuelva la siguiente información: deporte y cantidad de socios que lo practican, 
 ordenado en forma descendente por cantidad de socios.*/
@@ -66,4 +67,4 @@ db.socios.aggregate([
 	{ $sort: 
 		{count: -1}
 	}
-])
+]);
